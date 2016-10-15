8481da103a9bf933eca0dea5f6ef0d8ea0d10cd1
donando101a <donando101a@hotmail.com>
2016-10-15 15:37:47 -0400

    Comment changes.
    Minor comment changes and addListener and once method.

--- event-example.js
+++ event-example.js
@@ -1,6 +1,7 @@
-
-/* we need the events module 
-/* to create an eventEmitter object */
+/*------------------------------------------------------------------------------*/
+/* we need the events module                                                    */
+/* to create an eventEmitter object                                             */
+/*------------------------------------------------------------------------------*/
 var events = require('events');
 
 var myeventEmitter = new events.EventEmitter();
@@ -24,14 +25,20 @@ var dataHandler = function dataReceived() {
     console.log('>>>data received succesfully.<<<');    
     // do something with the data
 }
+
+var oneTime = function () {
+    console.log('>>>display a message only one time')
+}
 /*------------------------------------------------------------------------------*/
 /* Bindings:                                                                    */  
 /*------------------------------------------------------------------------------*/
-/* use method .on for binding to a Handler and                                  */
-/* use method .on for binding to an anonymous function                          */
+/* use method .on or .addListener for binding to a Handler and                  */
+/* use method .on for binding to a function                                     */
+/* use method .once for a listener that is invoked only once                    */
 /*------------------------------------------------------------------------------*/
-myeventEmitter.on('event_Connection', connectHandler);
-myeventEmitter.on('event_DataReceived', dataHandler); 
+myeventEmitter.addListener('event_Connection', connectHandler);
+myeventEmitter.on('event_DataReceived', dataHandler);
+myeventEmitter.once('event_OneTime', oneTime)
 /*------------------------------------------------------------------------------*/
 /* Mainline:                                                                    */  
 /*------------------------------------------------------------------------------*/
@@ -40,4 +47,5 @@ myeventEmitter.on('event_DataReceived', dataHandler);
 /*------------------------------------------------------------------------------*/
 // do a connection to something
 myeventEmitter.emit('event_Connection');
+myeventEmitter.emit('event_OneTime');
 console.log("Program Ended.");
