5a21d47d37262233df32b8e005ab36f96fc443c6
donando101a <donando101a@hotmail.com>
2016-10-17 16:21:28 -0400

    Added simple-web-server

--- event-example.js
+++ event-example.js
@@ -4,6 +4,7 @@
 var events = require('events');
 
 var myeventEmitter = new events.EventEmitter();
+myeventEmitter.setMaxListeners(10); //10 is default, 0 is for unlimited
 /*------------------------------------------------------------------------------*/
 /* Event Handlers:                                                              */                      
 /*      connectHandler - fire when a successful connection is made              */
@@ -19,6 +20,11 @@ var connectHandler = function connected() {
     // go get some data  
     myeventEmitter.emit('event_DataReceived');
 }
+var connectHandler2 = function connected2() {
+    console.log('>>>connection2 succesful.<<<'); 
+    // go get some data  
+    myeventEmitter.emit('event_DataReceived');
+}
 
 var dataHandler = function dataReceived() {
     console.log('>>>data received succesfully.<<<');    
@@ -29,6 +35,7 @@ var dataHandler = function dataReceived() {
 /*------------------------------------------------------------------------------*/
 /* use method .on for binding to a Handler and                                  */
 /* use method .on for binding to an anonymous function                          */
+/* method .listener is the same as method .on                                   */
 /*------------------------------------------------------------------------------*/
 myeventEmitter.on('event_Connection', connectHandler);
 myeventEmitter.on('event_DataReceived', dataHandler); 
@@ -40,4 +47,19 @@ myeventEmitter.on('event_DataReceived', dataHandler);
 /*------------------------------------------------------------------------------*/
 // do a connection to something
 myeventEmitter.emit('event_Connection');
+
+// count how many listeners for the event
+var count = myeventEmitter.listenerCount('event_Connection');
+console.log(count); 
+// remove a listener and count
+myeventEmitter.removeListener('event_Connection',connectHandler);
+var count = myeventEmitter.listenerCount('event_Connection');
+console.log(count); 
+// bind the listener again and bind event to a second listener, both get triggered
+myeventEmitter.addListener('event_Connection', connectHandler);
+myeventEmitter.addListener('event_Connection', connectHandler2);
+console.log(myeventEmitter.listenerCount('event_Connection'));
+
+myeventEmitter.emit('event_Connection'); // fire the event
+
 console.log("Program Ended.");
